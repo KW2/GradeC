38348c861f51d63d314273a24ff45ad34d9110b8
KW2 <dksk73@naver.com>
2017-12-27 13:47:58 +0900

    first commit

---
+++ app/src/main/java/com/kw2/kw2/gradec/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kw2.kw2.gradec;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
